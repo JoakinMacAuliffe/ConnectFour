554e9b28a5e524f8f50a7063aa30e40e2c7d448d
Sergio Pinto <sergio.pinto1@mail.udp.cl>
2025-06-10 14:06:36 -0400

    Update Game.java
    chupalpico

--- src/Game.java
+++ src/Game.java
@@ -1,1 +1,40 @@
+import java.util.Scanner;
 
+public class Game{
+    
+    private String status;
+    private String winnerPlayerName;
+    private String playerNameA;
+    private String playerNameB;
+    private ConnectFour connectFour;
+    
+    Scanner sc = new Scanner(System.in);
+    connectFour = new ConnectFour();
+    
+    Game(String playerNameA, String playerNameB){
+        
+        this.playerNameA = playerNameA;
+        this.playerNameB = playerNameB;
+        
+        status = "IN_PROGRESS";
+        winnerPlayerName = NULL;
+        
+    }
+    
+    public String Play(){
+        
+        while(winnerPlayerName == NULL){
+            
+            System.out.println("En que fila desea jugar su ficha? (1-7)")
+            answer = sc.nextInt() - 1;
+            
+            connectFour.makeMove(answer);
+            
+            if()
+            
+            
+        }
+        
+        
+    }
+}
