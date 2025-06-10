92460ae0e2849f801ea99327a41fe351754494f1
Martin Correa <166661033+mhcorreaa@users.noreply.github.com>
2025-06-10 14:04:04 -0400

    commit -1

---
+++ src/ConnectFour.java
@@ -0,0 +1,45 @@
+class ConnectFour{
+    
+    private char[7][6] grid;
+    private currentSymbol;
+    
+    public ConnectFour(){
+        
+        for(int i=0; i<7; i++){
+            
+            for(int j=0; j<6; j++){
+                
+                this.grid[i][j] = ' ';
+            }
+        }
+        
+        this.currentSymbol = x;
+    }
+    
+    public Boolean makeMove(int z){
+        
+        if(z >= 7 || z < 0) return false;
+        
+        int position = 0; //posicion en la columna
+        
+        while(position < 6 && grid[z][position] != ' ') position++;
+        
+        if(position != 6){
+            
+            grid[z][position] = currentSymbol;
+            
+            if(currentSymbol == 'x') currentSymbol = 'o';
+            else currentSymbol = 'x';
+            
+            return true;
+        }
+        
+        else return false;
+            
+    }
+    
+    public char isGameOver(){
+        
+        
+    }
+}
