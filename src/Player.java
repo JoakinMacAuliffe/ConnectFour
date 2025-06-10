91cdd6ee08b36c619317afe1edbef26b3b6935a2
Joakin Mac-Auliffe <jfmacauliffe@gmail.com>
2025-06-10 13:27:29 -0400

    chumbeque

---
+++ src/Player.java
@@ -0,0 +1,34 @@
+class Player {
+
+    private String playerName;
+    private int wins;
+    private int draws;
+    private int losses;
+
+    Player(String playerName, int wins int draws, int losses) {
+        this.playerName = playerName;
+        this.wins = wins;
+        this.draws = draws;
+        this.losses = losses;
+    }
+
+    public void addWin() {
+        wins++;
+    }
+
+    public void addDraw() {
+        draws++;
+    }
+
+    public void addLoss() {
+        losses++;
+    }
+
+    public double winRate() {
+        int gamesPlayed = wins + draws + losses;
+        if(gamesPlayed != 0) {
+
+        }
+    }
+
+}
