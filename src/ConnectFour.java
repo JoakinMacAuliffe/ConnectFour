aaaeab1576359cdbdfca71d286541cfa5df597ed
Martin Correa <166661033+mhcorreaa@users.noreply.github.com>
2025-06-13 00:21:13 -0400

    hecha la clase isGameOver (no la probe)

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -60,7 +60,7 @@ class ConnectFour{
                     && grid[i+3][j+3] == symbol) return symbol; //busca diagonal hacia arriba 
 
                 if(i+3 < 7 && j-3 < 6 && grid[i+1][j-1] == symbol && grid[i+2][j-2] == symbol
-                    && grid[i+3][j-3] == symbol) return symbol;
+                    && grid[i+3][j-3] == symbol) return symbol; //busca diagonal hacia abajo
             }
         }
 
